3b91025dca4bca9acc888f72f2e2bb11e95eebce
GiomaniaEdu <jorge.shigui123@gmail.com>
2024-10-09 16:03:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('rsvp-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const asistencia = document
+}
